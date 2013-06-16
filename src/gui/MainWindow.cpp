keepassxreboot/keepassxc

Settings UI integration.

src/gui/MainWindow.cpp
@@ -20,6 +20,7 @@
 
 #include <QtGui/QCloseEvent>
 #include <QtGui/QShortcut>
+#include <QtGui/QLineEdit>
 
 #include "autotype/AutoType.h"
 #include "core/Config.h"
@@ -32,12 +33,48 @@
 #include "gui/entry/EntryView.h"
 #include "gui/group/GroupView.h"
 
+#include "http/Service.h"
+#include "http/HttpSettings.h"
+#include "http/OptionDialog.h"
+#include "gui/SettingsWidget.h"
+
+class HttpPlugin: public ISettingsPage {
+public:
+    HttpPlugin(DatabaseTabWidget * tabWidget): m_service(new Service(tabWidget)) {
+    }
+    virtual ~HttpPlugin() {
+    }
+    virtual QString name() {
+        return QObject::tr("Http");
+    }
+    virtual QWidget * createWidget() {
+        OptionDialog * dlg = new OptionDialog();
+        QObject::connect(dlg, SIGNAL(removeSharedEncryptionKeys()), m_service.data(), SLOT(removeSharedEncryptionKeys()));
+        QObject::connect(dlg, SIGNAL(removeStoredPermissions()), m_service.data(), SLOT(removeStoredPermissions()));
+        return dlg;
+    }
+    virtual void loadSettings(QWidget * widget) {
+        qobject_cast<OptionDialog*>(widget)->loadSettings();
+    }
+    virtual void saveSettings(QWidget * widget) {
+        qobject_cast<OptionDialog*>(widget)->saveSettings();
+        if (HttpSettings::isEnabled())
+            m_service->start();
+        else
+            m_service->stop();
+    }
+private:
+    QScopedPointer<Service> m_service;
+};
+
 const QString MainWindow::BaseWindowTitle = "KeePassX";
 
 MainWindow::MainWindow()
     : m_ui(new Ui::MainWindow())
 {
     m_ui->setupUi(this);
+
+    m_ui->settingsWidget->addSettingsPage(new HttpPlugin(m_ui->tabWidget));
 
     setWindowIcon(filePath()->applicationIcon());
     QAction* toggleViewAction = m_ui->toolBar->toggleViewAction();

szsolt/keepassx

src/gui/MainWindow.cpp
@@ -20,6 +20,7 @@
 
 #include <QtGui/QCloseEvent>
 #include <QtWidgets/QShortcut>
+#include <QtWidgets/QLineEdit>
 
 #include "autotype/AutoType.h"
 #include "core/Config.h"
@@ -32,12 +33,48 @@
 #include "gui/entry/EntryView.h"
 #include "gui/group/GroupView.h"
 
+#include "http/Service.h"
+#include "http/HttpSettings.h"
+#include "http/OptionDialog.h"
+#include "gui/SettingsWidget.h"
+
+class HttpPlugin: public ISettingsPage {
+public:
+    HttpPlugin(DatabaseTabWidget * tabWidget): m_service(new Service(tabWidget)) {
+    }
+    virtual ~HttpPlugin() {
+    }
+    virtual QString name() {
+        return QObject::tr("Http");
+    }
+    virtual QWidget * createWidget() {
+        OptionDialog * dlg = new OptionDialog();
+        QObject::connect(dlg, SIGNAL(removeSharedEncryptionKeys()), m_service.data(), SLOT(removeSharedEncryptionKeys()));
+        QObject::connect(dlg, SIGNAL(removeStoredPermissions()), m_service.data(), SLOT(removeStoredPermissions()));
+        return dlg;
+    }
+    virtual void loadSettings(QWidget * widget) {
+        qobject_cast<OptionDialog*>(widget)->loadSettings();
+    }
+    virtual void saveSettings(QWidget * widget) {
+        qobject_cast<OptionDialog*>(widget)->saveSettings();
+        if (HttpSettings::isEnabled())
+            m_service->start();
+        else
+            m_service->stop();
+    }
+private:
+    QScopedPointer<Service> m_service;
+};
+
 const QString MainWindow::BaseWindowTitle = "KeePassX";
 
 MainWindow::MainWindow()
     : m_ui(new Ui::MainWindow())
 {
     m_ui->setupUi(this);
+
+    m_ui->settingsWidget->addSettingsPage(new HttpPlugin(m_ui->tabWidget));
 
     setWindowIcon(filePath()->applicationIcon());
     QAction* toggleViewAction = m_ui->toolBar->toggleViewAction();